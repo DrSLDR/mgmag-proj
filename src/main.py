01eaca984af875b87fd8dfec6e2020e0fb370500
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-05 18:56:02 +0200

    tried adding a GameEngine but failed

--- src/main.py
+++ src/main.py
@@ -1,61 +1,10 @@
 import sys, pygame
 from pgu import gui
 
-class Painter(gui.Widget):
-    def __init__(self,**params):
-        gui.Widget.__init__(self,**params)
-
-        self.surface = None
-        self.state = 0
-
-        from strings import Logo
-
-        self.cuzco = pygame.image.load(Logo.game)
-        self.ballrect = self.cuzco.get_rect()
-
-    def init(self,v):
-        self.surface = pygame.Surface((int(v['width']),int(v['height'])))
-        color = v['color']
-        if v['color'] == 'custom':
-            color = v['custom']
-        else: color = pygame.Color(color)
-        self.surface.fill(color)
-        self.overlay = pygame.Surface((int(v['width']),int(v['height'])),pygame.SRCALPHA,32)
-        self.repaint()
-
-    def event(self,e):
-        if not self.surface: return
-
-        if e.type == gui.MOUSEBUTTONDOWN:
-            print("MOOOOOOUUUUSEEEEEE")
-        if e.type == gui.MOUSEMOTION:
-            print("DANCE!!!!")
-        if e.type is gui.MOUSEBUTTONUP:
-            print("JUMPPP")
-
-    ##The Painter class has its own paint method to render the painting surface and overlay.
-    ##::
-    def paint(self,s):
-        size = width, height = 920, 840
-        speed = [1, 2]
-        self.ballrect = self.ballrect.move(speed)
-        if self.ballrect.left < 0 or self.ballrect.right > width:
-            speed[0] = -speed[0]
-        if self.ballrect.top < 0 or self.ballrect.bottom > height:
-            speed[1] = -speed[1]
-
-        black = 0, 0, 255 # which is blue
-        s.fill(black)
-        # YOU JUST GOT RECT!
-        s.blit(self.cuzco, self.ballrect)
-        pygame.display.flip()
-
-    ##
-
-class App(gui.Desktop):
-    def __init__(self,**params):
+class UserInterface(gui.Desktop):
+    def __init__(self,display):
         from dialogs import HelpDialog, QuitDialog, WelcomeDialog
-        gui.Desktop.__init__(self,**params)
+        gui.Desktop.__init__(self)
 
         self.connect(gui.QUIT,self.quit,None)
 
@@ -126,5 +75,91 @@ class App(gui.Desktop):
         self.painter.surface = pygame.image.load(self.fname)
         self.painter.repaint()
 
-app = App()
-app.run()
+class GameEngine(object):
+    def __init__(self, disp):
+        self.disp = disp
+        self.app = UserInterface(self.disp)
+        self.app.engine = self
+
+        self.logo = pygame.image.load(Logo.game)
+        self.ballrect = self.logo.get_rect()
+
+    # Pause the game clock
+    def pause(self):
+        self.clock.pause()
+
+    # Resume the game clock
+    def resume(self):
+        self.clock.resume()
+
+    def render(self, dest, rect):
+        size = width, height = 920, 840
+        speed = [1, 2]
+        self.ballrect = self.ballrect.move(speed)
+        if self.ballrect.left < 0 or self.ballrect.right > width:
+            speed[0] = -speed[0]
+        if self.ballrect.top < 0 or self.ballrect.bottom > height:
+            speed[1] = -speed[1]
+
+        black = 0, 0, 255 # which is blue
+        dest.fill(black)
+        # YOU JUST GOT RECT!
+        dest.blit(self.cuzco, self.ballrect)
+
+        def draw_clock(name, pt, radius, col, angle):
+            pygame.draw.circle(dest, col, pt, radius)
+            pygame.draw.line(dest, (0,0,0), pt, 
+                             (pt[0]+radius*math.cos(angle),
+                              pt[1]+radius*math.sin(angle)), 2)
+            tmp = self.font.render(name, True, (255,255,255))
+            dest.blit(tmp, (pt[0]-radius, pt[1]+radius+5))
+
+        # Draw the real time clock
+        angle = self.clock.get_real_time()*2*math.pi/10.0
+        draw_clock("Real time", (30,30), 25, (255,200,100), angle)
+
+        # Now draw the game clock
+        angle = self.clock.get_time()*2*math.pi/10.0
+        draw_clock("Game time", (90,30), 25, (255,100,255), angle)
+
+        return (rect,)
+
+    def run(self):
+        self.app.update()
+        pygame.display.flip()
+
+        self.font = pygame.font.SysFont("", 16)
+
+        self.clock = timer.Clock() #pygame.time.Clock()
+        done = False
+        while not done:
+            # Process events
+            for ev in pygame.event.get():
+                if (ev.type == pygame.QUIT or 
+                    ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE):
+                    done = True
+                else:
+                    # Pass the event off to pgu
+                    self.app.event(ev)
+            # Render the game
+            rect = self.app.get_render_area()
+            updates = []
+            self.disp.set_clip(rect)
+            lst = self.render(self.disp, rect)
+            if (lst):
+                updates += lst
+            self.disp.set_clip()
+
+            # Cap it at 30fps
+            self.clock.tick(30)
+
+            # Give pgu a chance to update the display
+            lst = self.app.update()
+            if (lst):
+                updates += lst
+            pygame.display.update(updates)
+            pygame.time.wait(10)
+
+disp = pygame.display.set_mode((800, 600))
+eng = GameEngine(disp)
+eng.run()
