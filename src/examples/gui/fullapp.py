eb85f6a116bbc58bf62ac83b2036e9bdc9169a63
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-05 15:19:20 +0200

    kill all the whitespace

--- src/examples/gui/fullapp.py
+++ src/examples/gui/fullapp.py
@@ -14,17 +14,17 @@ from pgu import gui
 class NewDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("New Picture...")
-        
+
         ##Once a form is created, all the widgets that are added with a name
         ##are added to that form.
         ##::
         self.value = gui.Form()
-        
+
         t = gui.Table()
-        
+
         t.tr()
         t.td(gui.Label("Size"),align=0,colspan=2)
-        
+
         tt = gui.Table()
         tt.tr()
         tt.td(gui.Label("Width: "),align=1)
@@ -35,14 +35,14 @@ class NewDialog(gui.Dialog):
         t.tr()
         t.td(tt,colspan=2)
         ##
-        
+
         t.tr()
         t.td(gui.Spacer(width=8,height=8))
         t.tr()
         t.td(gui.Label("Format",align=0))
         t.td(gui.Label("Background",align=0))
 
-        t.tr()        
+        t.tr()
         g = gui.Group(name="format",value="rgb")
         tt = gui.Table()
         tt.tr()
@@ -52,7 +52,7 @@ class NewDialog(gui.Dialog):
         tt.td(gui.Radio(g,value="bw"))
         tt.td(gui.Label(" Grayscale"),align=-1)
         t.td(tt,colspan=1)
-        
+
         g = gui.Group(name="color",value="#ffffff")
         tt = gui.Table()
         tt.tr()
@@ -62,24 +62,24 @@ class NewDialog(gui.Dialog):
         tt.td(gui.Radio(g,value="#ffffff"))
         tt.td(gui.Label(" White"),align=-1)
         tt.tr()
-        
+
         default = "#ffffff"
         radio = gui.Radio(g,value="custom")
         color = gui.Color(default,width=40,height=16,name="custom")
         picker = ColorDialog(default)
-        
+
         color.connect(gui.CLICK,gui.action_open,{'container':t,'window':picker})
         picker.connect(gui.CHANGE,gui.action_setvalue,(picker,color))
 
         tt.td(radio)
         tt.td(color)
-        
+
         t.td(tt,colspan=1)
-        
+
         t.tr()
         t.td(gui.Spacer(width=8,height=8))
-        
-        ##The okay button CLICK event is connected to the Dailog's 
+
+        ##The okay button CLICK event is connected to the Dailog's
         ##send event method.  It will send a gui.CHANGE event.
         ##::
         t.tr()
@@ -87,26 +87,26 @@ class NewDialog(gui.Dialog):
         e.connect(gui.CLICK,self.send,gui.CHANGE)
         t.td(e)
         ##
-        
+
         e = gui.Button("Cancel")
         e.connect(gui.CLICK,self.close,None)
         t.td(e)
-        
+
         gui.Dialog.__init__(self,title,t)
 class ColorDialog(gui.Dialog):
     def __init__(self,value,**params):
         self.value = list(gui.parse_color(value))
-        
+
         title = gui.Label("Color Picker")
-        
+
         main = gui.Table()
-        
+
         main.tr()
-        
+
         self.color = gui.Color(self.value,width=64,height=64)
         main.td(self.color,rowspan=3,colspan=1)
-        
-        ##The sliders CHANGE events are connected to the adjust method.  The 
+
+        ##The sliders CHANGE events are connected to the adjust method.  The
         ##adjust method updates the proper color component based on the value
         ##passed to the method.
         ##::
@@ -125,9 +125,9 @@ class ColorDialog(gui.Dialog):
         e = gui.HSlider(value=self.value[2],min=0,max=255,size=32,width=128,height=16)
         e.connect(gui.CHANGE,self.adjust,(2,e))
         main.td(e,2,2)
-                        
+
         gui.Dialog.__init__(self,title,main)
-        
+
     ##The custom adjust handler.
     ##::
     def adjust(self,value):
@@ -142,93 +142,93 @@ class ColorDialog(gui.Dialog):
 class AboutDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("About Cuzco's Paint")
-        
+
         width = 900
         height = 500
         doc = gui.Document(width=width)
-        
+
         space = title.style.font.size(" ")
-        
+
         doc.block(align=0)
-        for word in """Cuzco's Paint v1.0 by Phil Hassey""".split(" "): 
+        for word in """Cuzco's Paint v1.0 by Phil Hassey""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
         doc.br(space[1])
-        
+
         doc.block(align=-1)
         doc.add(gui.Image("../../../img/gravitas_logo.png"),align=1)
-        for word in """Cuzco's Paint is a revolutionary new paint program it has all the awesome features that you need to paint really great pictures.""".split(" "): 
+        for word in """Cuzco's Paint is a revolutionary new paint program it has all the awesome features that you need to paint really great pictures.""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
         doc.br(space[1])
-        
+
         doc.block(align=-1)
-        for word in """Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!""".split(" "): 
+        for word in """Cuzco's Paint will drive you wild!  Cuzco's Paint was made as a demo of Phil's Pygame Gui.  We hope you enjoy it!""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
-            
+
         for i in range(0,10):
             doc.block(align=-1)
             for word in """This text has been added so we can show off our ScrollArea widget.  It is a very nice widget built by Gal Koren!""".split(" "):
                 doc.add(gui.Label(word))
                 doc.space(space)
             doc.br(space[1])
-                
+
         gui.Dialog.__init__(self,title,gui.ScrollArea(doc,width,height))
 ##
 class HelpDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("Help")
-        
+
         doc = gui.Document(width=400)
-        
+
         space = title.style.font.size(" ")
-        
+
         doc.block(align=-1)
         doc.add(gui.Image("../../../img/gravitas_logo.png"),align=1)
-        for word in """Cuzco's Paint is a revolutionary new paint program it has all the awesome features that you need to paint really great pictures.""".split(" "): 
+        for word in """Cuzco's Paint is a revolutionary new paint program it has all the awesome features that you need to paint really great pictures.""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
         doc.br(space[1])
-        
-        doc.block(align=-1)    
-        for word in """This help isn't really here for any other reason than to have a help dialog.""".split(" "): 
+
+        doc.block(align=-1)
+        for word in """This help isn't really here for any other reason than to have a help dialog.""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
 
         gui.Dialog.__init__(self,title,doc)
-        
+
 class QuitDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("Quit")
-        
+
         t = gui.Table()
-        
+
         t.tr()
         t.add(gui.Label("Are you sure you want to quit?"),colspan=2)
-        
+
         t.tr()
         e = gui.Button("Okay")
         e.connect(gui.CLICK,self.send,gui.QUIT)
         t.td(e)
-        
+
         e = gui.Button("Cancel")
         e.connect(gui.CLICK,self.close,None)
         t.td(e)
-        
+
         gui.Dialog.__init__(self,title,t)
 
 class WelcomeDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("Welcome")
-        
+
         doc = gui.Document(width=400)
-        
+
         space = title.style.font.size(" ")
 
         doc.block(align=-1)
         doc.add(gui.Image("../../../img/gravyboat_logo_text_alt.png"),align=1)
-        for word in """Welcome to Cuzco's Paint.  Cuzco's Paint is a demo of the features of Phil's Pygame GUI.  Cuzco's Paint only supports saving in the .TGA format.""".split(" "): 
+        for word in """Welcome to Cuzco's Paint.  Cuzco's Paint is a demo of the features of Phil's Pygame GUI.  Cuzco's Paint only supports saving in the .TGA format.""".split(" "):
             doc.add(gui.Label(word))
             doc.space(space)
 
@@ -237,71 +237,71 @@ class WelcomeDialog(gui.Dialog):
 class OpenDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("Open Picture")
-        
+
         t = gui.Table()
-        
+
         self.value = gui.Form()
-        
+
         t.tr()
         t.td(gui.Label("Open: "))
         t.td(gui.Input(name="fname"),colspan=3)
-        
+
         t.tr()
         e = gui.Button("Okay")
         e.connect(gui.CLICK,self.send,gui.CHANGE)
         t.td(e,colspan=2)
-        
+
         e = gui.Button("Cancel")
         e.connect(gui.CLICK,self.close,None)
         t.td(e,colspan=2)
-        
+
         gui.Dialog.__init__(self,title,t)
 
 class SaveDialog(gui.Dialog):
     def __init__(self,**params):
         title = gui.Label("Save As...")
-        
+
         t = gui.Table()
-        
+
         self.value = gui.Form()
-        
+
         t.tr()
         t.td(gui.Label("Save: "))
         t.td(gui.Input(name="fname"),colspan=3)
-        
+
         t.tr()
         e = gui.Button("Okay")
         e.connect(gui.CLICK,self.send,gui.CHANGE)
         t.td(e,colspan=2)
-        
+
         e = gui.Button("Cancel")
         e.connect(gui.CLICK,self.close,None)
         t.td(e,colspan=2)
-        
+
         gui.Dialog.__init__(self,title,t)
 
 
 class Painter(gui.Widget):
     def __init__(self,**params):
         gui.Widget.__init__(self,**params)
-        
+
         self.surface = None
         self.state = 0
         self.cuzco = pygame.image.load("../../../img/gravitas_logo.png")
-        
+
     def init(self,v):
         self.surface = pygame.Surface((int(v['width']),int(v['height'])))
         color = v['color']
-        if v['color'] == 'custom': 
+        if v['color'] == 'custom':
             color = v['custom']
         else: color = pygame.Color(color)
         self.surface.fill(color)
         self.overlay = pygame.Surface((int(v['width']),int(v['height'])),pygame.SRCALPHA,32)
         self.repaint()
-        
+
     def event(self,e):
         if not self.surface: return
-        
+
         if e.type == gui.MOUSEBUTTONDOWN:
             if hasattr(self,app.mode.value+"_down"):
                 action = getattr(self,app.mode.value+"_down")
@@ -314,14 +314,14 @@ class Painter(gui.Widget):
             if hasattr(self,app.mode.value+"_up"):
                 action = getattr(self,app.mode.value+"_up")
                 action(e)
-    
+
     ##The Painter class has its own paint method to render the painting surface and overlay.
     ##::
     def paint(self,s):
         s.blit(self.surface,(0,0))
         s.blit(self.overlay,(0,0))
     ##
-                            
+
     def draw_down(self,e):
         self.state = 1
         self.pos = e.pos
@@ -333,7 +333,7 @@ class Painter(gui.Widget):
         self.repaint()
     def draw_up(self,e):
         self.state = 0
-        
+
     def cuzco_down(self,e):
         self.state = 1
         self.cuzco_motion(e)
@@ -348,7 +348,7 @@ class Painter(gui.Widget):
         self.surface.blit(self.overlay,(0,0))
         self.overlay.fill((0,0,0,0))
         self.repaint()
-        
+
     def box_down(self,e):
         self.state = 1
         self.pos = e.pos
@@ -363,8 +363,8 @@ class Painter(gui.Widget):
         self.surface.blit(self.overlay,(0,0))
         self.overlay.fill((0,0,0,0))
         self.repaint()
-        
-    
+
+
     def circle_down(self,e):
         self.state = 1
         self.pos = e.pos
@@ -385,20 +385,20 @@ class Painter(gui.Widget):
         self.surface.blit(self.overlay,(0,0))
         self.overlay.fill((0,0,0,0))
         self.repaint()
-        
-        
+
+
 
 class App(gui.Desktop):
     def __init__(self,**params):
         gui.Desktop.__init__(self,**params)
-        
+
         self.connect(gui.QUIT,self.quit,None)
-        
+
         c = gui.Container(width=940,height=980)
         spacer = 8
-        
+
         self.fname = 'untitled.tga'
-        
+
         self.new_d = NewDialog()
         self.new_d.connect(gui.CHANGE,self.action_new,None)
         self.open_d = OpenDialog()
@@ -407,10 +407,10 @@ class App(gui.Desktop):
         self.save_d.connect(gui.CHANGE,self.action_saveas,None)
         self.quit_d = QuitDialog()
         self.quit_d.connect(QUIT,self.quit,None)
-        
+
         self.help_d = HelpDialog()
         self.about_d = AboutDialog()
-        
+
         ##Initializing the Menus, we connect to a number of Dialog.open methods for each of the dialogs.
         ##::
         menus = gui.Menus([
@@ -426,7 +426,7 @@ class App(gui.Desktop):
         c.add(menus,0,0)
         menus.rect.w,menus.rect.h = menus.resize()
         #print 'menus',menus.rect
-        
+
         ##We utilize a Toolbox.  The value of this widget determins how drawing is done in the Painter class.
         ##::
         self.mode = mode = gui.Toolbox([
@@ -440,11 +440,11 @@ class App(gui.Desktop):
         mode.rect.x,mode.rect.y = mode.style.x,mode.style.y
         mode.rect.w,mode.rect.h = mode.resize()
         #mode._resize()
-        
+
         default = "#000000"
         self.color = color = gui.Color(default,width=mode.rect.w,height=mode.rect.w)
         self.color_d = ColorDialog(default)
-        
+
         color.connect(gui.CLICK,self.color_d.open,None)
 
         # Updates the toolbox color picker with the value in the color dialog box
@@ -464,28 +464,28 @@ class App(gui.Desktop):
 
         welcome_d = WelcomeDialog()
         self.connect(gui.INIT,welcome_d.open,None)
-        
+
         self.widget = c
-        
+
     def action_new(self,value):
         self.new_d.close()
         self.fname = 'untitled.tga'
         self.painter.init(self.new_d.value.results())
-        
+
     def action_save(self,value):
         pygame.image.save(self.painter.surface,self.fname)
-        
+
     def action_saveas(self,value):
         self.save_d.close()
         self.fname = self.save_d.value['fname'].value
         pygame.image.save(self.painter.surface,self.fname)
-        
+
     def action_open(self,value):
         self.open_d.close()
         self.fname = self.open_d.value['fname']
         self.painter.surface = pygame.image.load(self.fname)
         self.painter.repaint()
-        
-        
+
+
 app = App()
 app.run()
