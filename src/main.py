43d84310595b0b2ec4bab214c087eb95a77ffd0f
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-05 17:55:56 +0200

    deleted comments

--- src/main.py
+++ src/main.py
@@ -1,29 +1,5 @@
 import sys, pygame
 from pgu import gui
-#pygame.init()
-
-#size = width, height = 920, 840
-#speed = [1, 2]
-#black = 0, 0, 255
-
-#screen = pygame.display.set_mode(size)
-
-#ball = pygame.image.load("../img/gravitas_logo.png")
-#ballrect = ball.get_rect()
-
-#while 1:
-    #for event in pygame.event.get():
-        #if event.type == pygame.QUIT: sys.exit()
-
-    #ballrect = ballrect.move(speed)
-    #if ballrect.left < 0 or ballrect.right > width:
-        #speed[0] = -speed[0]
-    #if ballrect.top < 0 or ballrect.bottom > height:
-        #speed[1] = -speed[1]
-
-    #screen.fill(black)
-    #screen.blit(ball, ballrect)
-    #pygame.display.flip()
 
 class Painter(gui.Widget):
     def __init__(self,**params):
