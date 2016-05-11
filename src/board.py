a2205da94f1da3abe00ba0b519c1f72aa184cd94
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-11 15:47:55 +0200

    Added some very fancy wrong math

--- src/board.py
+++ src/board.py
@@ -7,17 +7,6 @@ class PositionCalculator:
     """
     def __init__(self, size):
         self.size = size
-        def unit(value):
-            return 2.5-(value/len(tiles))
-        def smallerThen(this,value,elsefunction):
-            def func(x):
-                if x < this:
-                    return value
-                else:
-                    return elsefunction(x)
-
-            return func
-        self.scaleFactor = unit
         pass
 
     """
@@ -26,11 +15,47 @@ class PositionCalculator:
     """
     def calcPosition(self,tileNumber):
         import math
+        # explenation: https://math.stackexchange.com/questions/163080/on-a-two-dimensional-grid-is-there-a-formula-i-can-use-to-spiral-coordinates-in
+        # tileSize = n from explenation
+        spiralSize = abs(math.sqrt(tileNumber)) # (m in explenation)
+        spirStep = 0 # k in explenation
+        if spiralSize % 2 == 1: # is odd
+            spirStep = (spiralSize-1)/2
+        elif tileNumber >= (spiralSize**2 + spiralSize): # n >= m(m+1)
+            spirStep = spiralSize/2
+        else:
+            spirStep = spiralSize/2-1
+
         Point = namedtuple('Point', ['x', 'y'])
-        power = self.scaleFactor(tileNumber)
+        factor = 60
+        if 2*spirStep *(2*spirStep + 1) < tileNumber <= (2*spirStep + 1) ** 2:
+            print("bleh")
+            return Point(
+                x=(tileNumber-4*spirStep**2-3*spirStep)*factor,
+                y=spirStep*factor
+            )
+        if (2*spirStep + 1)**2 < tileNumber <= 2*(spirStep+1)*(2*spirStep+1):
+            print("bluh")
+            return Point(
+                x=(spirStep+1)*factor,
+                y=(4*spirStep**2+5*spirStep+1-tileNumber)*factor
+            )
+        if 2*(spirStep + 1)*(2*spirStep+1) < tileNumber <= 4*(spirStep+1)**2:
+            print("bloh")
+            return Point(
+                x=(4*spirStep**2+7*spirStep+3-tileNumber)*factor,
+                y=(-spirStep-1)*factor
+            )
+        if 4*(spirStep + 1)**2 < tileNumber <= 2*(spirStep+1)*(2*spirStep+3):
+            print("blah")
+            return Point(
+                x=(-spirStep-1)*factor,
+                y=(tileNumber-4*spirStep**2-9*spirStep-5)*factor,
+            )
+
         return Point(
-            x=(self.size.width / tileNumber) * math.cos(tileNumber**(1/power)),
-            y=(self.size.height / tileNumber) * math.sin(tileNumber**(1/power))
+            x=0,
+            y=0
         )
 class Renderer:
     def __init__(self, screenSize):
@@ -41,5 +66,5 @@ class Renderer:
     def render(self, font):
         for i in tiles:
             pos = self.calulator(i+1)
-            pos = (pos[0]+self.screenSize.width/2, pos[1]++self.screenSize.height/2)
+            pos = (pos[0]+self.screenSize.width/2, pos[1]+self.screenSize.height/2)
             font("%i"%i, pos)
