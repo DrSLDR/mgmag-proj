be46ec364ce09a309b2015a183e4c5b727492688
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-22 12:40:33 +0200

    Deleted a bunch of unused GUI code

--- gravitas/main.py
+++ gravitas/main.py
@@ -4,27 +4,18 @@
 import pygame
 from pgu import gui, timer
 
-
 class DrawingArea(gui.Widget):
     def __init__(self, width, height):
         gui.Widget.__init__(self, width=width, height=height)
         self.imageBuffer = pygame.Surface((width, height))
-
     def paint(self, surf):
         # Paint whatever has been captured in the buffer
         surf.blit(self.imageBuffer, (0, 0))
-
     # Call self function to take a snapshot of whatever has been rendered
     # onto the display over self widget.
     def save_background(self):
         disp = pygame.display.get_surface()
         self.imageBuffer.blit(disp, self.get_abs_rect())
-
-class TestDialog(gui.Dialog):
-    def __init__(self):
-        title = gui.Label("Some Dialog Box")
-        label = gui.Label("Close self window to resume.")
-        gui.Dialog.__init__(self, title, label)
 
 """It describes all the buttons and stuff like that. This is
 where pgu comes in,"""
@@ -38,95 +29,22 @@ class MainGui(gui.Desktop):
     def __init__(self, disp):
         gui.Desktop.__init__(self)
         container = gui.Container()
-
         # Setup the 'game' area where the action takes place
         self.gameArea = DrawingArea(disp.get_width(),
                                     self.gameAreaHeight)
         # Setup the gui area
         self.menuArea = gui.Container(
             height=disp.get_height()-self.gameAreaHeight)
-
         tbl = gui.Table(height=disp.get_height())
         tbl.tr()
         tbl.td(self.gameArea)
         tbl.tr()
         tbl.td(self.menuArea)
-
-        #self.setup_menu(container)
         import human_player
         human_player.App(self.menuArea)
         
         container.add(tbl,0,0)
         self.init(container, disp)
-
-    def setup_menu(self):
-        tbl = gui.Table(vpadding=5, hpadding=2)
-        tbl.tr()
-
-        dlg = TestDialog()
-
-        def dialog_cb():
-            dlg.open()
-
-        btn = gui.Button("Modal dialog", height=50)
-        btn.connect(gui.CLICK, dialog_cb)
-        tbl.td(btn)
-
-        # Add a button for pausing / resuming the game clock
-        def pause_cb():
-            if (self.engine.clock.paused):
-                self.engine.resume()
-            else:
-                self.engine.pause()
-
-        btn = gui.Button("Pause/resume clock", height=50)
-        btn.connect(gui.CLICK, pause_cb)
-        tbl.td(btn)
-
-        # Add a slider for adjusting the game clock speed
-        tbl2 = gui.Table()
-
-        timeLabel = gui.Label("Clock speed")
-
-        tbl2.tr()
-        tbl2.td(timeLabel)
-
-        slider = gui.HSlider(value=23,min=0,max=100,size=20,height=16,width=120)
-
-        def update_speed():
-            self.engine.clock.set_speed(slider.value/10.0)
-
-        slider.connect(gui.CHANGE, update_speed)
-
-        tbl2.tr()
-        tbl2.td(slider)
-
-        tbl.td(tbl2)
-
-        self.menuArea.add(tbl, 0, 0)
-
-    def open(self, dlg, pos=None):
-        # Gray out the game area before showing the popup
-        rect = self.gameArea.get_abs_rect()
-        dark = pygame.Surface(rect.size).convert_alpha()
-        dark.fill((0,0,0,150))
-        pygame.display.get_surface().blit(dark, rect)
-        # Save whatever has been rendered to the 'game area' so we can
-        # render it as a static image while the dialog is open.
-        self.gameArea.save_background()
-        # Pause the gameplay while the dialog is visible
-        running = not(self.engine.clock.paused)
-        self.engine.pause()
-        gui.Desktop.open(self, dlg, pos)
-        while (dlg.is_open()):
-            for ev in pygame.event.get():
-                self.event(ev)
-            rects = self.update()
-            if (rects):
-                pygame.display.update(rects)
-        if (running):
-            # Resume gameplay
-            self.engine.resume()
 
     def get_render_area(self):
         return self.gameArea.get_abs_rect()
@@ -168,20 +86,15 @@ class GameEngine(object):
 
     def render(self, dest, rect):
         size = width, height = rect.width, rect.height
-
         backgroundColor = 0, 0, 255 # which is blue
         dest.fill(backgroundColor)
-
         import math
         def font(text, position, color=(255,255,255)):
             tmp = self.font.render(text, True, color)
             dest.blit(tmp, position)
-
         self.gameManager.update()
         self.renderBoard(font, disp)
-
         return (rect,)
-
 
     def run(self):
         self.app.update()
