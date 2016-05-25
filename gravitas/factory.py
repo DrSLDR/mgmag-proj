e619f74661b16385e08e1bbcea2ce39f399b7866
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-25 14:17:41 +0200

    organize the controllers

--- gravitas/factory.py
+++ gravitas/factory.py
@@ -7,7 +7,8 @@ import random, json
 from model.player import Player, Ship
 from model.card import Card, Deck
 from gamemanager import State, GameManager
-from player_controller import RandomAI_PC, Human_PC
+from controller.human import Human_PC
+from controller.random import RandomAI_PC
 
 class Factory():
     """ A factory which can create playerControllers, states and the Gamemanager"""
