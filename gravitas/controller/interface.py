949f2079c7df707f117088fa3a678d03f3a9bc35
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-25 20:22:14 +0200

    exception based dynamic dispatch

--- gravitas/controller/interface.py
+++ gravitas/controller/interface.py
@@ -7,20 +7,18 @@ class IPlayerController():
     def __init__(self, player, args):
         self.player = player
 
-    #TODO replace return none with: 
-    # raise NotImplementedError("Subclasses should implement this!")
     def pollDraft(self, state):
         """Function that returns the choice of a stack from the draw field"""
-        return None # returns None because it is an abstract function
+        raise NotImplementedError("Subclasses should implement this!")
 
     def pollPlay(self, state):
         """Function that returns which card the PC want to play"""
-        return None # returns None because it is an abstract function
+        raise NotImplementedError("Subclasses should implement this!")
 
     def pollEmergencyStop(self, state):
         """Function that returns the choice of using the emergency stop as a boolean"""
-        return None # returns None because it is an abstract function
+        raise NotImplementedError("Subclasses should implement this!")
 
     def informReveal(self, cards):
         """Informs the PlayerController of the cards which have been played"""
-        return None # returns None because it is an abstract function
+        raise NotImplementedError("Subclasses should implement this!")
