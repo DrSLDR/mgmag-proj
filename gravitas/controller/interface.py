f534bba2e4f3568efc1aaf881dd373321ea37f78
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-26 14:19:17 +0200

    better errorhandling of the interface

--- gravitas/controller/interface.py
+++ gravitas/controller/interface.py
@@ -7,18 +7,29 @@ class IPlayerController():
     def __init__(self, player, args):
         self.player = player
 
+    def _NotImplementError(self):
+        """Throws an error that a subclass should implement a function
+        It uses some black magic to tell which class and what function
+        """
+        import inspect
+        raise NotImplementedError(
+            "%s should implement %s!" % (
+                type(self).__name__, inspect.stack()[1][3]
+            )
+        )
+
     def pollDraft(self, state):
         """Function that returns the choice of a stack from the draw field"""
-        raise NotImplementedError("Subclasses should implement this!")
+        self._NotImplementError()
 
     def pollPlay(self, state):
         """Function that returns which card the PC want to play"""
-        raise NotImplementedError("Subclasses should implement this!")
+        self._NotImplementError()
 
     def pollEmergencyStop(self, state):
         """Function that returns the choice of using the emergency stop as a boolean"""
-        raise NotImplementedError("Subclasses should implement this!")
+        self._NotImplementError()
 
     def informReveal(self, cards):
         """Informs the PlayerController of the cards which have been played"""
-        raise NotImplementedError("Subclasses should implement this!")
+        self._NotImplementError()
