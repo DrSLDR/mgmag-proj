9977e82cee67bd2c26c11d5e1989e77d8d433843
Jappie Klooster <jappieklooster@hotmail.com>
2016-05-27 19:35:41 +0200

    implement it as a controller, delete unused code

--- gravitas/controller/neural.py
+++ gravitas/controller/neural.py
@@ -1,6 +1,34 @@
 """A player controller that uses a neural network to determine the next move"""
 
+from .interface import RandomAI_PC
+
 import tensorflow as tf
+class Neurotic_PC(RandomAI_PC):
+    """We extend the random AI so that we don't have to implement everything"""
+    def __init__(self, player, args,container):
+        super().__init__(player,args, container)
+        self.playNetwork = Factory.createPlay()
+
+    def pollPlay(self, state):
+        enemies = [x for x in state.players if x.getPos() != player.getPos()]
+        feed_dict = {
+            "player_position:0": self.player.getPos(),
+            "enemy_position_0:0":enemies[0],
+            "enemy_position_1:0":enemies[1],
+            "enemy_position_2:0":enemies[2],
+            "hulk_position_0:0":state.hulks[0].getPos(),
+            "hulk_position_1:0":state.hulks[1].getPos(),
+        }
+        for (i,card) in enumerate(self.player.getHand()):
+            feed_dict["card_%i_value" % i] = card.getValue()
+            feed_dict["card_%i_effect" % i] = card.getType()
+            feed_dict["card_%i_play_order" % i] = ord(card.getName())[0]
+        with tf.Session() as sess:
+            prefrences = enumerate(sess.run(self.playNetwork, feed_dict=feed_dict))
+            from operator import itemgetter
+            prefrences = list(sorted(prefrences, key=itemgetter(1)))
+            print(prefrences)
+        raise RuntimeError("Neural network failed, we shouldn't reach here")
 
 from collections import namedtuple
 
@@ -16,84 +44,7 @@ Input=namedtuple('Input', ['source', 'index'])
 from enum import Enum
 Sources = Enum('Sources', 'input operation')
 
-# Considered architectures:
-# 1. a matrix of inputs, the problem was that you cannot get a reduced value,
-# reliably out of this with random modiification to the graph
-# 2. All integer inputs, this is the one I went for
-#     1. A single output node that did some summing and then use a modulo to
-#        figure out which card, the problem is that a modulo is extremly complex
-#     2. Preference based output, each card gets a output node which determines,
-#        Its preference. I like this better
-# Then I also considered dropping the resolution order and sorting the cards
-# before hand, I chose not to do this, because it relies to much on the
-# semantics of the network, I don't know how important resolution order is.
-# But I may come back on this one.
-#
-# The final argument will be a pack command, which jams all output nodes in an
-# array so the network only has to consulted once.
-
-class NeuralNetwork:
-    def createPlay():
-        builder = NeuralNetworkBuilder()
-        for i in range(0,8):
-            builder.addInput("card_%i_value" % i, 1)
-            builder.addInput("card_%i_effect" % i, 0)
-            builder.addInput("card_%i_play_order" % i, 0)
-        builder.addInput("player_position")
-        for i in range(0,3):
-            builder.addInput("enemy_position_%i" % i)
-        for i in range(0,2):
-            builder.addInput("hulk_position_%i" % i)
-        for i in range(0,8):
-            builder.outputNodes.append(Input(Sources.input, i))
-        builder.operationNodes.append(Operation(
-                tensorFunction=tf.add,
-                arguments=[
-                    Input(Sources.input, 3),
-                    Input(Sources.input, len(builder.inputNodes)-1)
-                ]
-        ))
-        builder.outputNodes[3] = Input(Sources.operation, 0)
-        return builder.createOutputTensor()
-
-    def cardToTupple(card):
-        from collections import namedtuple
-        Card=namedtupple('Card', ['type, value, name'])
-        return Card(type=card.getType(), value=card.getValue(),
-                    # we need to have the same name datatype,
-                    # only the first letter decides the order, we don't care
-                    # about the value since it won't matter for the neural
-                    # network (it only matters that the values are different)
-                    name=ord(card.getName()[0]) 
-                    )
-
-    def process(self, cards):
-        """
-        Use the neural network to proccess the gamestate/mode.
-        In the gamestate and mode (ie pollrequest number)
-        Out the choice (a number)
-        """
-        #TODO
-        # 1. creation and proccing sperate
-        # 2. Sort cards on play order
-        cards = map(NeuralNetwork.cardToTupple,cards)
-        #values = list(map(lambda x: ,reduce(list.__add__,cards)))
-
-
-def run():
-    with tf.Session() as sess:
-        work = NeuralNetwork.createPlay()
-        print(sess.run(work, feed_dict={
-            "player_position:0": 3,
-            "enemy_position_0:0":10,
-            "enemy_position_1:0":10,
-            "enemy_position_2:0":10,
-            "hulk_position_0:0":10,
-            "hulk_position_1:0":10,
-        }))
-
-
-class NeuralNetworkBuilder:
+class Builder:
     """The tensorflow neuralnetworks are immutable, this means we need
     to define a structure around it to do neuro evolution (since we want
     to change the graph)
@@ -113,6 +64,8 @@ class NeuralNetworkBuilder:
     def addInput(self, name, value=None):
         self.inputNodes.append(InputNode(name=name, default_value=value))
 
+    def addOpperation(self, function, arguments):
+        self.operationNodes.append(Operation(function, arguments))
     def _inputTuppleToTensor(tupple):
         if tupple.default_value is None:
             return tf.placeholder(tf.int32, shape=[], name=tupple.name)
@@ -121,10 +74,19 @@ class NeuralNetworkBuilder:
         )
 
     def createOutputTensor(self):
-        """Creates the neural network based on the configured processing steps"""
+        """Creates the output tensor which is a preference list,
+
+        Goes trough the input nodes first to create tensors from them,
+        then defines the resolve function which can resolve the input tupple
+        (ie another tensor), from there the operations are build one by one.
+
+        after that the output nodes are build and finally put into a pack
+        tensor. Using a pack tensor allows us to execute just this tensor
+        rather than calling the neural network multiple times
+        """
         # to create we just go back over the network
         inputTensors = list(
-            map(NeuralNetworkBuilder._inputTuppleToTensor, self.inputNodes)
+            map(Builder._inputTuppleToTensor, self.inputNodes)
         )
         operationTensors = []
         tensors = {
@@ -141,3 +103,26 @@ class NeuralNetworkBuilder:
                 )
             )
         return tf.pack([resolveInput(x) for x in self.outputNodes])
+
+class Factory:
+    def createPlay():
+        """Creates a sparsely connected play network"""
+        builder = Builder()
+        for i in range(0,8):
+            builder.addInput("card_%i_value" % i, 1)
+            builder.addInput("card_%i_effect" % i, 0)
+            builder.addInput("card_%i_play_order" % i, 0)
+        builder.addInput("player_position")
+        for i in range(0,3):
+            builder.addInput("enemy_position_%i" % i)
+        for i in range(0,2):
+            builder.addInput("hulk_position_%i" % i)
+        for i in range(0,8):
+            builder.outputNodes.append(Input(Sources.input, i))
+        builder.addOpperation(tf.add, [
+                Input(Sources.input, 3),
+                Input(Sources.input, len(builder.inputNodes)-1)
+            ]
+        )
+        builder.outputNodes[3] = Input(Sources.operation, 0)
+        return builder.createOutputTensor()
